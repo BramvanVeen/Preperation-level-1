9650e4a5665665fee41a7ca7873f9b858bc66828
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-06-08 14:55:58 +0200

    Add project files.

---
+++ Preperation level 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Preperation_level_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
